209d660928dbc126a3adca0bb3c96929c9a350fb
Saurabh Singh <50754992+saurabhsingh17@users.noreply.github.com>
2024-06-12 22:56:15 +0530

    Update KadaneAlgorithm.java

--- KadaneAlgorithm.java
+++ KadaneAlgorithm.java
@@ -1,5 +1,7 @@
-class Solution {
-    public int maxSubArray(int[] nums) {
+class MaxSubArray {
+
+    //BruteForce O(N^2)
+    public int bruteForce(int[] nums) {
         int n = nums.length;
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
@@ -8,6 +10,18 @@ class Solution {
                 sum += nums[j];
                 maxSum = Math.max(maxSum, sum);
             }
+        }
+        return maxSum;
+    }
+    //Kadane Algorithm O(N)
+    public int optimal(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < n; i++) {
+            sum+= nums[i];
+            if (sum < 0) sum = 0;
+            maxSum = Math.max(maxSum, sum);
         }
         return maxSum;
     }
