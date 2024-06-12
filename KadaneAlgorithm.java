ff0862532fb21b071518947bffc58dea854c8752
Saurabh Singh <50754992+saurabhsingh17@users.noreply.github.com>
2024-06-12 23:45:28 +0530

    Update KadaneAlgorithm.java

--- KadaneAlgorithm.java
+++ KadaneAlgorithm.java
@@ -25,4 +25,26 @@ class MaxSubArray {
         }
         return maxSum;
     }
+    // Return the left and right index of the max subarray sum,
+    // assuming there's exactly one result (no ties).
+    // Sliding window variation of Kadane's: O(n)
+    public static int[] slidingWindow(int[] nums) {
+        int maxSum = nums[0];
+        int curSum = 0;
+        int maxL = 0, maxR = 0;
+        int L = 0;
+
+        for (int R = 0; R < nums.length; R++) {
+            if (curSum < 0) {
+                curSum = 0;
+                L = R;
+            }
+            curSum += nums[R];
+            if (curSum > maxSum) {
+                maxSum = curSum;
+                maxL = L; 
+                maxR = R;     
+            }    
+        }    
+        return new int[] {maxL, maxR};
 }
