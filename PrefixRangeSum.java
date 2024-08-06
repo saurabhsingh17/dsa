da04c465dc9506675f334b052b54d22f24e6d23b
saurabhsingh <saurabh623a@gmail.com>
2024-08-06 23:03:46 +0530

    Day-2

--- PrefixRangeSum.java
+++ PrefixRangeSum.java
@@ -1,33 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class PrefixRangeSum {
-    int[] prefixSum;
-    int n;
+    private int[] prefixSum;
 
     public static void main(String[] args) {
         int nums[] = {-2, 0, 3, -5, 2, -1};
         PrefixRangeSum prefixRangeSum = new PrefixRangeSum(nums);
-         System.out.println(prefixRangeSum.rangeSum(0, 2));
-         System.out.println(prefixRangeSum.rangeSum(2,5));
-         System.out.println(prefixRangeSum.rangeSum(0, 5));
-
+        System.out.println(prefixRangeSum.rangeSum(0, 2)); // Output: 1
+        System.out.println(prefixRangeSum.rangeSum(2, 5)); // Output: -1
+        System.out.println(prefixRangeSum.rangeSum(0, 5)); // Output: -3
     }
 
     public PrefixRangeSum(int[] nums) {
-        n = nums.length;
+        int n = nums.length;
         prefixSum = new int[n];
-        int total = 0;
-        int i = 0;
-        for (int n : nums) {
-            total += n;
-            prefixSum[i++] = total;
+        if (n > 0) {
+            prefixSum[0] = nums[0];
+            for (int i = 1; i < n; i++) {
+                prefixSum[i] = prefixSum[i - 1] + nums[i];
+            }
         }
     }
 
     public int rangeSum(int left, int right) {
-        int preRight = prefixSum[right];
-        int preLeft = left > 0 ? prefixSum[left-1] : 0;
-        return (preRight - preLeft);
+        if (left == 0) {
+            return prefixSum[right];
+        } else {
+            return prefixSum[right] - prefixSum[left - 1];
+        }
     }
 }
