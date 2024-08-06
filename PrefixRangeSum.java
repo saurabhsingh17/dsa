3f3586cf8279a58448dbb8960cf4fbd32f733236
saurabhsingh <saurabh623a@gmail.com>
2024-08-06 22:58:25 +0530

    Day-2

--- PrefixRangeSum.java
+++ PrefixRangeSum.java
@@ -2,20 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrefixRangeSum {
-    List<Integer> prefix;
+    int[] prefixSum;
+    int n;
+
+    public static void main(String[] args) {
+        int nums[] = {-2, 0, 3, -5, 2, -1};
+        PrefixRangeSum prefixRangeSum = new PrefixRangeSum(nums);
+         System.out.println(prefixRangeSum.rangeSum(0, 2));
+         System.out.println(prefixRangeSum.rangeSum(2,5));
+         System.out.println(prefixRangeSum.rangeSum(0, 5));
+
+    }
 
     public PrefixRangeSum(int[] nums) {
-        prefix = new ArrayList<>();
+        n = nums.length;
+        prefixSum = new int[n];
         int total = 0;
+        int i = 0;
         for (int n : nums) {
             total += n;
-            prefix.add(total);
+            prefixSum[i++] = total;
         }
     }
 
     public int rangeSum(int left, int right) {
-        int preRight = prefix.get(right);
-        int preLeft = left > 0 ? prefix.get(left - 1) : 0;
+        int preRight = prefixSum[right];
+        int preLeft = left > 0 ? prefixSum[left-1] : 0;
         return (preRight - preLeft);
     }
 }
